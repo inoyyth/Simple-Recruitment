44d2e9b7a98755bd434b278e2b8e40d3323c2d18
DIVISI IT <divisiit.ghm@gmail.com>
2024-04-02 15:27:45 +0700

    create migration table pelamar, dokumen pelamar, role, dan jadwal

---
+++ database/migrations/2024_04_01_052032_create_pelamar_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pelamar', function (Blueprint $table) {
+            $table->increments('id_pelamar');
+            $table->string('no_ktp', 16);
+            $table->string('nama_pelamar', 100);
+            $table->date('tanggal_lahir');
+            $table->longText('alamat');
+            $table->string('email', 100);
+            $table->text('password');
+            $table->string('no_hp', 15);
+            $table->enum('jenis_kelamin', ['L','P']);
+            $table->text('foto');
+            $table->timestamps();
+            $table->timestamp('deleted_ad');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pelamar');
+    }
+};
