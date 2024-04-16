360d0bf6655a99d867a4a60e56908d87f19f87dd
Roni <zonieremong@gmail.com>
2024-04-16 15:53:09 +0700

    Make CRUT Controller Peserta, Controller Role, Controller User, Create Models Peserta, Models Role, Models User, API

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -11,4 +11,5 @@ class Role extends Model
 
     protected $table = 'role';
     protected $primarykey = 'id_role';
+    protected $fillable = ['title'];
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -17,10 +17,15 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
+
     protected $fillable = [
-        'name',
+        'nip',
+        'nama',
         'email',
         'password',
+        'jenis_kelamin',
+        'no_hp',
+        'alamat'
     ];
 
     /**
